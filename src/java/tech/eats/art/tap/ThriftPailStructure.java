93d313692528a4886c70f27ec13cfe1e65670ccc
John Hunter <john@push-it-forward.com>
2016-11-28 11:28:26 -0800

    added ser and des methods to thriftpailstructure

--- src/java/tech/eats/art/tap/ThriftPailStructure.java
+++ src/java/tech/eats/art/tap/ThriftPailStructure.java
@@ -1,7 +1,9 @@
 package tech.eats.art.tap;
 
 import backtype.hadoop.pail.PailStructure;
+import org.apache.thrift.TBase;
 import org.apache.thrift.TDeserializer;
+import org.apache.thrift.TException;
 import org.apache.thrift.TSerializer;
 
 import java.util.List;
@@ -9,23 +11,43 @@ import java.util.List;
 /**
  * Created by jhunter on 11/28/16.
  */
-public class ThriftPailStructure<T extends Comparable> implements PailStructure<T> {
+public abstract class ThriftPailStructure<T extends Comparable> implements PailStructure<T> {
 
     private transient TSerializer ser;
     private transient TDeserializer des;
 
-    
+    public TSerializer getSerializer() {
+        if(ser == null) ser = new TSerializer();
+        return ser;
+    }
+
+    public TDeserializer getDeserializer() {
+        if(ser == null) des = new TDeserializer();
+        return des;
+    }
 
     public boolean isValidTarget(String... strings) {
         return false;
     }
 
-    public T deserialize(byte[] bytes) {
-        return null;
+    public T deserialize(byte[] record) {
+        T ret = createThriftObject();
+        try{
+            getDeserializer().deserialize((TBase)ret, record);
+        }catch(TException e){
+            throw new RuntimeException(e);
+        }
+        return ret;
     }
 
-    public byte[] serialize(T t) {
-        return new byte[0];
+
+
+    public byte[] serialize(T obj) {
+        try{
+            return getSerializer().serialize((TBase) obj);
+        }catch (TException e){
+            throw new RuntimeException(e);
+        }
     }
 
     public List<String> getTarget(T t) {
@@ -35,4 +57,6 @@ public class ThriftPailStructure<T extends Comparable> implements PailStructure<
     public Class getType() {
         return null;
     }
+
+    protected abstract T createThriftObject();
 }
