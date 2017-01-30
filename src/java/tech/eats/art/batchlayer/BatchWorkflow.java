abb4fe88cf63a3e99f3d007d844aad5cd3d7c3a0
John Hunter <john@push-it-forward.com>
2017-01-29 21:31:09 -0800

    added shred method

--- src/java/tech/eats/art/batchlayer/BatchWorkflow.java
+++ src/java/tech/eats/art/batchlayer/BatchWorkflow.java
@@ -3,6 +3,7 @@ package tech.eats.art.batchlayer;
 import backtype.cascading.tap.PailTap;
 import backtype.hadoop.pail.Pail;
 import cascading.tap.Tap;
+import cascalog.ops.RandLong;
 import jcascalog.Api;
 import jcascalog.Subquery;
 import org.apache.hadoop.conf.Configuration;
@@ -41,7 +42,7 @@ public class BatchWorkflow {
     }
 
 
-    public static PailTap attributeTaop(String path, final DataUnit._Fields... fields){
+    public static PailTap attributeTap(String path, final DataUnit._Fields... fields){
         PailTap.PailTapOptions opts = new PailTap.PailTapOptions();
         opts.attrs = new List[] {
                 new ArrayList<String>(){{
@@ -52,8 +53,29 @@ public class BatchWorkflow {
         };
         return new PailTap(path, opts);
     }
+    
+    public static Pail shred() throws IOException {
+        PailTap source = new PailTap("/tmp/swa/snapshot");
+        PailTap sink = splitDataTap("/tmp/swa/shredded");
+
+        Subquery reduced = new Subquery("?rand", "?data")
+                .predicate(source, "_", "?data-in")
+                .predicate(new RandLong())
+                .out("?rand");
+
+        Api.execute(sink, new Subquery("?data")
+            .predicate(reduced, "_", "?data"));
+
+        Pail shreddedPail = new Pail("/tmp/swa/shredded");
+        shreddedPail.consolidate();
+        return shreddedPail;
+    }
+
+    private static PailTap splitDataTap(String s) {
+    }
 
     private static void appendNewDataToMasterPail(Pail masterPail, Pail snapshotPail) {
+
     }
 
 }
