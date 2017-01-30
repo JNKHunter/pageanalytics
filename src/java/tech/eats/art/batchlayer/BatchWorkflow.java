e7316a176beba75ec2ba326d2c8f2d1e11ab2e63
John Hunter <john@push-it-forward.com>
2017-01-29 18:52:43 -0800

    added ingest method

--- src/java/tech/eats/art/batchlayer/BatchWorkflow.java
+++ src/java/tech/eats/art/batchlayer/BatchWorkflow.java
@@ -1,7 +1,12 @@
 package tech.eats.art.batchlayer;
 
+import backtype.hadoop.pail.Pail;
 import jcascalog.Api;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,11 +15,26 @@ import java.util.Map;
  */
 public class BatchWorkflow {
 
+    private final static String NEW_DATA_SNAPSHOT = "/tmp/swa/newDataSnapshot";
+
     public static void setApplicationConf(){
         Map conf = new HashMap();
         String sers = "backtype.hadoop.ThriftSerialization," +
                 "org.apache.hadoop.io.serializer.WritableSerialization";
         conf.put("io.serializations", sers);
         Api.setApplicationConf(conf);
+    }
+
+    public static void ingest(Pail masterPail, Pail newDataPail) throws IOException {
+        FileSystem fs = FileSystem.get(new Configuration());
+        fs.delete(new Path("/tmp/swa"), true);
+        fs.mkdirs(new Path("tmp/swa"));
+
+        Pail snapshotPail = newDataPail.snapshot(NEW_DATA_SNAPSHOT);
+        appendNewDataToMasterPail(masterPail, snapshotPail);
+    }
+
+    private static void appendNewDataToMasterPail(Pail masterPail, Pail snapshotPail) {
+
     }
 }
